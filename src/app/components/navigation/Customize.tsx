63791d7b450e4009bdfc920b117ea51cbbfc5513
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-01-18 15:36:15 -0300

    feat(Customize Tab): add and remove links added

--- src/app/components/navigation/Customize.tsx
+++ src/app/components/navigation/Customize.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '../buttons/Button'
 import { Separator } from '@/components/ui/separator'
 import Image from 'next/image'
@@ -7,20 +7,37 @@ import LinkCreation from '../LinkCreation'
 
 type Props = {}
 
+interface ILinks {
+    id: number
+    platform: string;
+    link: string;
+}
+
 const Customize = (props: Props) => {
-    const [links, setLinks] = useState([{ platform: "GitHub", link: "http://github.com/ugabb" }, { platform: "GitHub", link: "http://github.com/ugabb" }])
+    const [links, setLinks] = useState<ILinks[]>([])
+
+    const handleAddLink = (link: ILinks) => {
+        setLinks(prev => [...prev, link])
+    }
+    const handleRemoveLink = (id: number) => {
+        setLinks(prev => prev.filter(platform => platform.id !== id))
+    }
+
+    useEffect(() => {
+        console.log(links)
+    }, [links])
     return (
         <div className="flex flex-col gap-10 p-5 m-3 rounded-xl bg-white">
             <div className="flex flex-col gap-3">
                 <h3 className="text-2xl font-bold">Customize your links</h3>
                 <p className="text-sm text-grey">Add/edit/remove links below and then share all your profiles with the world!</p>
             </div>
-            <Button variant="outline" name="+ Add new link" />
+            <Button variant="outline" name="+ Add new link" onClick={() => handleAddLink({ id: links.length + 1, link: '', platform: '' })} />
 
-            {links ?
+            {links.length > 0 ?
                 <div className="flex flex-col gap-8">
-                    {links.map(link => (
-                        <LinkCreation />
+                    {links.map((link, i) => (
+                        <LinkCreation length={i + 1} removeLink={() => handleRemoveLink(link.id)} />
                     ))}
                 </div>
                 :
@@ -36,7 +53,7 @@ const Customize = (props: Props) => {
 
             <div className="flex flex-col gap-3">
                 <Separator />
-                <Button name='Save' variant='default' disabled />
+                <Button name='Save' variant='default' disabled={links.length > 0 ? false : true} />
             </div>
         </div>
     )
