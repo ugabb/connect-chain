81adbd7630405eb4cfd4ffef053277c40398c0ce
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-01-26 15:04:26 -0300

    feat: dinamic data from user session

--- src/app/components/navigation/ProfileDetails.tsx
+++ src/app/components/navigation/ProfileDetails.tsx
@@ -1,14 +1,17 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect } from 'react'
 import TextField from '../inputs/TextField'
 import { Separator } from '@/components/ui/separator'
 import Button from '../buttons/Button'
 import { useForm } from 'react-hook-form'
+import { useSession } from 'next-auth/react'
 
 type Props = {}
 
 const ProfileDetails = (props: Props) => {
+    const { data: session } = useSession()
     const { register } = useForm()
+
     return (
         <div className="flex flex-col gap-10 p-5 m-3 rounded-xl bg-white">
             <div className="flex flex-col gap-3">
@@ -17,22 +20,40 @@ const ProfileDetails = (props: Props) => {
             </div>
 
             <form className='flex flex-col gap-5'>
-                <div className="flex flex-col gap-3 bg-lightGrey p-5 rounded-lg">
-                    <p className='text-grey'>Profile picture</p>
-                    <label className="relative flex flex-col justify-center items-center w-[190px] h-[190px] cursor-pointer p-20 bg-lightPurple rounded-lg group  hover:shadow-lg hover:shadow-purple/30 transition-all ease-in">
-                        <input type="file" className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer" />
-                        <span className="p-2 text-purple font-bold rounded-md text-nowrap flex flex-col justify-center items-center">
-                            <Image src={'/images/icon-upload-image.svg'} height={40} width={40} alt='upload image' className='group-hover:text-lightPurple' />
-                            + Upload Image
-                        </span>
-                    </label>
-                    <p className='text-grey text-xs'>Image must be below 1024x1024px. Use PNG or JPG format.</p>
-                </div>
+                {session?.user.profileImage ?
+                    <div className="flex flex-col gap-3 bg-lightGrey p-5 rounded-lg">
+                        <p className='text-grey'>Profile picture</p>
+
+                        <label className="relative flex flex-col justify-center items-center w-[190px] h-[190px] cursor-pointer p-20 bg-lightPurple rounded-lg group  hover:shadow-lg hover:shadow-purple/30 transition-all ease-in">
+                            <input type="file" className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer" />
+                            <span className="p-2 text-purple font-bold rounded-md text-nowrap flex flex-col justify-center items-center">
+                                foi img
+                                <Image src={session.user.profileImage} height={40} width={40} alt='upload image' className='group-hover:text-lightPurple' />
+                                + Upload Image
+                            </span>
+                        </label>
+                        <p className='text-grey text-xs'>Image must be below 1024x1024px. Use PNG or JPG format.</p>
+                    </div>
+                    :
+
+                    <div className="flex flex-col gap-3 bg-lightGrey p-5 rounded-lg">
+                        <p className='text-grey'>Profile picture</p>
+
+                        <label className="relative flex flex-col justify-center items-center w-[190px] h-[190px] cursor-pointer p-20 bg-lightPurple rounded-lg group  hover:shadow-lg hover:shadow-purple/30 transition-all ease-in">
+                            <input type="file" className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer" />
+                            <span className="p-2 text-purple font-bold rounded-md text-nowrap flex flex-col justify-center items-center">
+                                <Image src={'/images/icon-upload-image.svg'} height={40} width={40} alt='upload image' className='group-hover:text-lightPurple' />
+                                + Upload Image
+                            </span>
+                        </label>
+                        <p className='text-grey text-xs'>Image must be below 1024x1024px. Use PNG or JPG format.</p>
+                    </div>
+                }
 
                 <div className="flex flex-col gap-3 bg-lightGrey p-5 rounded-lg">
                     <label className='text-xs'>
                         First Name*
-                        <TextField register={register} registerName='firstName' icon='none' />
+                        <TextField register={register} registerName='firstName' icon='none' value={session?.user.name} />
                     </label>
                     <label className='text-xs'>
                         Last Name*
@@ -40,7 +61,7 @@ const ProfileDetails = (props: Props) => {
                     </label>
                     <label className='text-xs'>
                         Email*
-                        <TextField register={register} registerName='email' icon='none' />
+                        <TextField register={register} registerName='email' icon='none' value={session?.user.email} />
                     </label>
                 </div>
             </form>
