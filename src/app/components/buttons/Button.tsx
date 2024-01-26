f2b49c52de5aaf18c1d683893caa697df4fabf12
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-01-26 15:30:49 -0300

    fix: button size on mobile

--- src/app/components/buttons/Button.tsx
+++ src/app/components/buttons/Button.tsx
@@ -7,7 +7,7 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 const Button: React.FC<Props> = ({ name, variant, ...props }) => {
     return (
-        <button className={`${variant === "outline" ? "border border-purple text-purple hover:bg-purple/10 hover:text-lightGrey" : "bg-purple text-white"} w-full text-md rounded-lg font-bold px-9 py-3 hover:bg-purple/80 transition-all ease-in-out disabled:opacity-30`} {...props}>{name}</button>
+        <button className={`${variant === "outline" ? "border border-purple text-purple hover:bg-purple/10 hover:text-lightGrey" : "bg-purple text-white"} w-full text-xs md:text-md rounded-lg font-bold px-9 py-3 hover:bg-purple/80 transition-all ease-in-out disabled:opacity-30 text-nowrap`} {...props}>{name}</button>
     )
 }
 
