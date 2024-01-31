e90b7a4b0ecdcbbf3aa8a5970ae807047d99d5ae
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-01-31 10:27:55 -0300

    fix: onblur event fixed

--- src/app/components/inputs/TextField.tsx
+++ src/app/components/inputs/TextField.tsx
@@ -12,17 +12,17 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
 
 const TextField: React.FC<Props> = ({ icon, register, registerName, ...props }: Props) => {
     const [onFocus, setOnFocus] = useState<boolean>(false);
-
+    
     return (
-        <div className={`flex items-center border rounded-lg w-full h-10 bg-white overflow-hidden py-2 px-3 gap-3 ${onFocus ? 'border-purple shadow-lg shadow-purple/10' : 'border-greyBorder'} `}>
+        <div className={`flex items-center border rounded-lg w-full h-10 bg-white overflow-hidden py-2 px-3 gap-3 hover:border-purple hover:shadow-lg shadow-purple/10  ${onFocus ? 'border-purple shadow-lg shadow-purple/10' : 'border-greyBorder'} `}>
             {icon !== "none" && <Image width={12} height={12} alt='link icon' src={icon ? `/images/${icon}` : "/images/icon-link.svg"} />}
 
             <input
                 className='w-full outline-none'
+                {...props}
+                {...register(registerName)}
                 onFocus={() => setOnFocus(true)}
                 onBlur={() => setOnFocus(false)}
-                {...register(registerName)}
-                {...props}
             />
         </div>
     );
